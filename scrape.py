e6fcb866cb210d4a5ce7312e9ee753e5df99c3cb
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2023-10-17 14:26:38 -0400

    Updated scrape function to include upc in return

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from dotenv import load_dotenv
 import time
 import os
+import re
 
 load_dotenv(override=True)
 
@@ -17,11 +18,10 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--headless")
 options.add_argument(f"--user-agent={USER_AGENT}")
 
-driver = webdriver.Chrome(options=options)
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
-
-def scrape_product_info_by_upc(upc: int) -> dict:
+def scrape_product_info_by_upc(upc: str) -> dict:
+    driver = webdriver.Chrome(options=options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.get(f"https://www.meijer.com/shopping/search.html?text={upc}")
     time.sleep(2)
 
@@ -33,6 +33,8 @@ def scrape_product_info_by_upc(upc: int) -> dict:
 
         prod_price = driver.find_element(By.CLASS_NAME, value="product-tile__regular-price-text")
         product_info["price"] = prod_price.text.replace('$', '')
+
+        product_info["upc"] = upc
     except NoSuchElementException:
         return product_info
 
@@ -49,12 +51,32 @@ def parse_product_info(title: str) -> dict:
     if "-" in title:
         info = title.split("-")
         name = info[0].strip()
-    else:
+    elif "," in title:
         info = title.split(",")
         name = info[0].strip()
+    else:
+        if "\n" in title:
+            name = title.split("\n")
+            name = name[0]
+        else:
+            name = title.strip()
+        return {"name": name}
 
-    description = info[-1].strip().split(" ")
-    qty = description[0].strip()
-    units = description[1].strip()
+    if " " in info[-1].strip():
+        description = info[-1].strip().split(" ")
 
-    return {"name": name, "qty": qty, "units": units}
+        if re.search("[A-z]+", description[0]):
+            qty = re.findall("\\d+", description[0])
+            units = re.findall("[A-z]+", description[0])
+            return {"name": name, "qty": qty[0], "units": units[0]}
+        else:
+            qty = description[0].strip()
+            units = description[1].strip().replace(".", "")
+            return {"name": name, "qty": qty, "units": units}
+    else:
+        description = info[-1].strip()
+        qty = re.findall("\\d+", description)
+        units = re.findall("[A-z]+", description)
+        return {"name": name, "qty": qty[0], "units": units[0]}
+
+
