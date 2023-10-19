d3496ff633d01df99e7f77e5d2a769bca70f1bb8
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2023-10-19 15:23:48 -0400

    Finished script to log in and download receipts from start date.

--- download_receipts.py
+++ download_receipts.py
@@ -17,15 +17,22 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("useAutomationExtension", False)
 options.add_argument("--disable-blink-features=AutomationControlled")
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument(f"--user-agent={USER_AGENT}")
+options.add_argument("--window-size=1920,1080")
+options.add_experimental_option("prefs", {
+    "download.default_directory": "./receipts",
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+})
 
 driver = webdriver.Chrome(options=options)
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
 
-# clicks on receipt and takes a screenshot then closes window
-async def take_receipt_screenshot(receipt, date):
+# clicks on receipt and downloads pdf then renames file with date of purchase
+async def download_receipt_pdf(receipt, date):
+    print("Getting receipt for date: " + date)
     receipt.find_element(By.CLASS_NAME, "order-receipt__show-pdf").click()
     time.sleep(2)
     current_window = driver.current_window_handle
@@ -35,8 +42,10 @@ async def take_receipt_screenshot(receipt, date):
         if window != current_window:
             date = date.replace("/", "_")
             driver.switch_to.window(window)
-            driver.save_screenshot(f"./receipts/{date}.png")
-            time.sleep(2)
+            old_pdf_name = "./receipts/meijer_digital_receipt.pdf"
+            new_file_name = f"./receipts/{date}.pdf"
+            os.rename(old_pdf_name, new_file_name)
+            print("File downloaded...")
             driver.close()
             driver.switch_to.window(current_window)
 
@@ -45,6 +54,7 @@ async def take_receipt_screenshot(receipt, date):
 # goes through every receipt on page until it locates one with a date that is older than starting date
 # clicks on receipt and waits until screenshot is taken before continuing down the page
 async def download_meijer_receipts(starting_date: str) -> None:
+    print("Starting process...")
     start_date = datetime.strptime(starting_date, "%m/%d/%Y")
 
     driver.get("https://www.meijer.com/")
@@ -53,6 +63,7 @@ async def download_meijer_receipts(starting_date: str) -> None:
     driver.find_element(By.CLASS_NAME, "meijer-header__account-signin-button").click()
     time.sleep(2)
 
+    print("Logging in...")
     driver.find_element(By.XPATH, '//button[text()="Sign In"]').click()
     time.sleep(2)
 
@@ -60,12 +71,14 @@ async def download_meijer_receipts(starting_date: str) -> None:
     driver.find_element(By.ID, "input35").send_keys(MEIJER_PASSWORD)
     driver.find_element(By.CLASS_NAME, "button").click()
     time.sleep(2)
+    print("Logged in...")
 
     driver.find_element(By.CLASS_NAME, "meijer-header__account-signin-button").click()
     driver.find_element(By.XPATH, '//a[text()="My Orders & Receipts"]').click()
     time.sleep(2)
 
     driver.find_element(By.XPATH, '//button[text()="In-Store Receipts"]').click()
+    print("Finding receipts...")
     time.sleep(2)
 
     receipt_list = driver.find_elements(By.CLASS_NAME, "order-receipt__list-tile")
@@ -79,9 +92,10 @@ async def download_meijer_receipts(starting_date: str) -> None:
         receipt_date = datetime.strptime(date, "%m/%d/%Y")
 
         if receipt_date.date() > start_date.date():
-            await take_receipt_screenshot(receipt, date)
+            await download_receipt_pdf(receipt, date)
 
+    print("Downloads finished...")
     driver.quit()
 
 
-asyncio.run(download_meijer_receipts(starting_date="10/01/2023"))
+asyncio.run(download_meijer_receipts(starting_date="09/01/2023"))
