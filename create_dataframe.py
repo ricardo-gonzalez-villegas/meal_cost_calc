14cfcef842e16b7c75c4eaffe412df0d3662115d
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2023-10-22 16:31:28 -0400

    Made simple script to create csv from data

--- create_dataframe.py
+++ create_dataframe.py
@@ -1,28 +1,12 @@
+from populate_lists import populate_lists
+from datetime import date
 import pandas as pd
 
-upc = list()
-name = list()
-qty = list()
-units = list()
-price = list()
-sale_price = list()
+lists = populate_lists()
+today = date.today().strftime("%m/%d/%y")
 
+df = pd.DataFrame(lists, columns=['UPC', 'Name', 'Qty', 'Units', 'Price', 'Sale_Price'])
 
-def populate_lists(product_info: dict) -> None:
-    if len(product_info) == 0:
-        return
-    upc.append(product_info["upc"])
-    name.append(product_info["name"])
-    if "qty" in product_info.keys():
-        qty.append(product_info["qty"])
-    else:
-        qty.append("")
-    if "units" in product_info.keys():
-        units.append(product_info["units"])
-    else:
-        units.append("")
-    price.append(product_info["price"])
-    if "sale_price" in product_info.keys():
-        sale_price.append(product_info["sale_price"])
-    else:
-        sale_price.append("")
+df.to_csv('./product_info.excel.csv', index=False)
+
+print("Finished")
